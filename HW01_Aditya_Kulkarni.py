74414b1dc88e8f0571524bee2a02e2f435997e2b
Aditya Kulkarni <40800556+BeardedAmbivert@users.noreply.github.com>
2020-01-28 19:49:46 -0500

    Added a few comments

--- HW01_Aditya_Kulkarni.py
+++ HW01_Aditya_Kulkarni.py
@@ -17,6 +17,7 @@ def play_game() -> None:
     while True:
         human_move: str = input("Choose your option 'R', 'P', 'S' or 'Q' for quitting: ").upper()
         computer_move: str = choice(list(mappings.keys()))
+        #Checking for quit, invalid inputs and tie breakers
         if human_move == 'Q':
             print("Let's play later")
             break
@@ -26,6 +27,7 @@ def play_game() -> None:
         elif mappings[human_move] == mappings[computer_move]:
             print(f"It's a tie. {mappings[human_move]} -> {mappings[computer_move]}. Wanna try again? ")
         else:
+            #Checking for winner 
             if options[mappings[human_move]] == mappings[computer_move]:
                 print(f"Awesome! You've won. {mappings[human_move]} -> {mappings[computer_move]}")
             else:
